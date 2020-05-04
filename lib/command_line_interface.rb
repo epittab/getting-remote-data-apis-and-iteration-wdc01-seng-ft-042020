966b9a11e72dae1e81cfc15e355587fa564b2f33
epittab <ep7m@me.com>
2020-05-04 13:56:29 -0400

    aimproved user input handling functionality

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,6 +7,9 @@ def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
   input = gets.chomp
+  if input.empty?
+    input = get_character_from_user
+  end
   #puts "this was your input: #{input}"
   return input
 end
